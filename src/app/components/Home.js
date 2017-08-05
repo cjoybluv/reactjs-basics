7031a3bb9a4cf75f8a7e70e0c794d82ef54dd700
Dave Hemmer <dave_hemmer@hotmail.com>
2017-08-05 00:58:21 -0700

    00-work along with videos-not routing

---
+++ src/app/components/Home.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Home extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      age: props.initialAge,
+      status: 0,
+      homeLink: props.initialLinkName
+    };
+    setTimeout(() => {
+        this.setState({
+            status: 1
+        });
+    },3000);
+    console.log("Constructor");
+  }
+
+  componentWillMount() {
+      console.log("Component will Mount");
+  }
+  
+  componentDidMount() {
+      console.log("Component did mount!")
+  }
+  
+  componentWillReceiveProps(nextProps) {
+      console.log("Component will receive props",nextProps);
+  }
+  
+  shouldComponentUpdate(nextProps, nextState) {
+      console.log("Should Component update", nextProps, nextState);
+      // if (nextState.status === 1) {
+      //     return false;
+      // }
+      return true;
+  }
+  
+  componentWillUpdate(nextProps, nextState) {
+      console.log("Component Will Update", nextProps, nextState);
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+      console.log("Component Did Update", prevProps, prevState);
+  }
+  
+  componentWillUnmount() {
+      console.log("Component Will Unmount");
+  }
+  
+  onMakeOlder() {
+    this.setState({
+        age: this.state.age + 3
+    });
+  }
+  
+  onChangeLink() {
+      this.props.changeLink(this.state.homeLink);
+  }
+
+  onHandleChange(event) {
+    this.setState({
+       homeLink: event.target.value
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <p>In a new Component!</p>
+        <p>Your name is: {this.props.name}, you are {this.state.age} years old!</p>
+        <p>Status: {this.state.status} </p>
+        <hr/>
+        <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
+        <hr/>
+        <button onClick={this.props.greet} className="btn btn-info">Greet</button>
+        <hr/>
+        <input type="text"
+               value={this.state.homeLink}
+               onChange={(event) => this.onHandleChange(event)}/>
+        <button onClick={this.onChangeLink.bind(this)} className="btn btn-success">Change Link</button>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    greet: PropTypes.func,
+    initialLinkName: PropTypes.string
+};
